a64a4ee45eb308fd75bdf09c2eb1f0119e2f15fd
qxy <18735359200@163.com>
2016-04-19 18:27:09 +0800

    漏了

--- testRef/ViewController.m
+++ testRef/ViewController.m
@@ -7,17 +7,56 @@
 //
 
 #import "ViewController.h"
+#import "MJRefresh.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
 
+
+@property (nonatomic,assign)BOOL  isLogind;
+
+@property (nonatomic,strong)UIScrollView * scrollView;
+
+
+@property (nonatomic,strong)UIImageView * imageView;
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIScrollView * scrollView = [[UIScrollView alloc]initWithFrame:self.view.bounds];
+    scrollView.contentSize = CGSizeMake(kScreenWidth, kScreenHeight+.4);
+    [self.view addSubview:scrollView];
+    
+    self.scrollView = scrollView;
+    
+    __block ViewController * mySelf = self;
+    [self.scrollView addHeaderWithCallback:^{
+        [NSTimer scheduledTimerWithTimeInterval:1 target:mySelf selector:@selector(closeRef) userInfo:nil repeats:NO];
+    }];
+    
+    [self.scrollView addFooterWithCallback:^{
+        [NSTimer scheduledTimerWithTimeInterval:1 target:mySelf selector:@selector(closeRef) userInfo:nil repeats:NO];
+
+    }];
+    
+    UIImageView * imageView = [[UIImageView alloc]initWithFrame:self.view.bounds];
+    imageView.image = [UIImage imageNamed:@"4.jpg"];
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    [scrollView addSubview:imageView];
+    
 }
+- (void)closeRef
+{
+    [self.scrollView headerEndRefreshing];
+    [self.scrollView footerEndRefreshing];
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
